84e9224ea08e8449cd15d34136c32eb9e83a5b1f
LilianKalkman <liliankalkman@hotmail.com>
2018-02-07 12:22:35 +0100

    added scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,10 @@
 class Movie < ApplicationRecord
   def flop?
-    total_gross == 5 || total_gross.blank? 
+    total_gross == 5 || total_gross.blank?
   end
+
+  def self.released
+    Movie.where('released_on < ?', Time.now).order('released_on desc')
+  end
+
 end
