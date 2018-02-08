080ce842504c40061a9c261b41aecabf65601d1c
LilianKalkman <liliankalkman@hotmail.com>
2018-02-08 10:58:49 +0100

    validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,16 @@
 class Movie < ApplicationRecord
+
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :title, :released_on, :duration, presence: true
+  validates :description, length: {minimum: 3}
+  validates :total_gross, numericality: {greater_than_or_equal_to: 0}
+  validates :image_file_name, allow_blank: true, format: {
+    with:    /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
+  validates :rating, inclusion: {in: RATINGS}
+
   def flop?
     total_gross == 5 || total_gross.blank?
   end
